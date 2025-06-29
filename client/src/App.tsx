5b2fdf8ded7d59e1b3dacc299ccd86472201af48
davehimself <itsdavehimself@users.noreply.github.com>
2025-06-28 19:46:46 -0500

    Add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,35 @@
 import "./App.css";
-import RoundForm from "./containers/RoundForm";
-import Rounds from "./containers/Rounds";
+import { Route, Routes } from "react-router";
+import Home from "./containers/Home";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import { useAppDispatch } from "./app/hooks";
+import { useEffect } from "react";
+import { fetchUser } from "./app/slices/authSlice";
+import Dashboard from "./containers/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
   return (
-    <>
-      <Rounds />
-      <RoundForm />
-    </>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/dashboard"
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
 
