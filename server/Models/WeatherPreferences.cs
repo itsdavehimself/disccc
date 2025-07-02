3cae40adab069b8f5664deba153a7a365e51ea94
davehimself <itsdavehimself@users.noreply.github.com>
2025-07-01 19:42:26 -0500

    Add properties

--- server/Models/WeatherPreferences.cs
+++ server/Models/WeatherPreferences.cs
@@ -2,7 +2,9 @@ namespace server.Models;
 
 public class WeatherPreferences
 {
-  public string PreferredConditions { get; set; } = string.Empty;
+  public bool AllowRain { get; set; }
+  public bool AllowSnow { get; set; }
+  public string CloudCoverPreference { get; set; } = string.Empty;
   public int MaxTempF { get; set; }
   public int MinTempF { get; set; }
   public int MaxWindSpeedMph { get; set; }
